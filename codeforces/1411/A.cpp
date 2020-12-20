ff3c30ef3b8034dcae90d9ad0866f517fa02c3eb
Timofey Chicherin <chicherintim@gmail.com>
2020-12-20 18:18:00 +0300

    Add solution for problem `A - In-game Chat` Link: https://codeforces.com/contest/1411/problem/A Tags: implementation, *800 Ref: https://codeforces.com/contest/1411/submission/101870622

---
+++ codeforces/1411/A.cpp
@@ -0,0 +1,58 @@
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <numeric>
+#define pb push_back
+#define str to_string
+#define len length
+#define mxel max_element
+#define mnel min_element
+#define sum accumulate
+using namespace std;
+using vi = vector<int>;
+
+int bin_search(vi v, int value) {
+    int start = 0;
+    int end = v.size() - 1;
+    int mid = start + (end - start) / 2;
+    while(v[mid] != value) {
+        if(start == end) {
+            mid = -1;
+            break;
+        }
+
+        if(v[mid] > value) end = mid - 1;
+        else start = mid + 1;
+        mid = start + (end - start) / 2;
+    }
+    return mid;
+}
+
+
+
+void oc(vi v) {
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i];
+        if(i != v.size() - 1) cout << " ";
+        else cout << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t; cin >> t;
+    while(t--) {
+        int n; cin >> n;
+        int firstN = n;
+        string s; cin >> s;
+        int cnt = 0;
+        while(n--) {
+            char c = s[n];
+            if(c == ')') cnt++;
+            else break;
+        }
+        cout << ((firstN - cnt) < (firstN / 2 + (firstN % 2))  ? "Yes" : "No") << endl;
+    }
+    return 0;
+}
