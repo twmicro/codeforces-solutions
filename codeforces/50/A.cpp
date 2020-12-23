be6d61f0740fca0e600fa971f018b466a897c6c0
Timofey Chicherin <chicherintim@gmail.com>
2020-12-23 16:31:00 +0300

    Add solution for problem `A - Domino piling` Link: https://codeforces.com/contest/50/problem/A Tags: greedy, math, *800 Ref: https://codeforces.com/contest/50/submission/102121081

---
+++ codeforces/50/A.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <algorithm>
+#include <bits/stdc++.h>
+#include <numeric>
+#define pb push_back
+#define str to_string
+#define len length
+#define mxel max_element
+#define mnel min_element
+#define sum accumulate
+#define FORI(n, step) for(int i = 0; i < n; i += step)
+#define FORJ(n, step) for(int j = 0; j < n; j += step)
+#define FORX(n, step) for(int x = 0; x < n; x += step)
+#define FORY(n, step) for(int y = 0; y < n; y += step)
+#define FORU(n, step) for(int u = 0; u < n; u += step)
+#define FORV(n, step) for(int v = 0; v < n; v += step)
+#define EACHI(n) FORI(n, 1)
+#define EACHJ(n) FORJ(n, 1)
+#define EACHX(n) FORX(n, 1)
+#define EACHY(n) FORY(n, 1)
+#define EACHZ(n) FORZ(n, 1)
+#define EACHU(n) FORU(n, 1)
+#define EACHV(n) FORV(n, 1)
+using namespace std;
+using vi = vector<int>;
+using graph = vector<vi>;
+using ll = long long;
+
+
+void oc(vi v) {
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i];
+        if(i != v.size() - 1) cout << " ";
+        else cout << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int m, n; cin >> n >> m;
+    cout << m * n / 2 << endl;
+    return 0;
+}
